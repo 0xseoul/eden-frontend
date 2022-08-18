0c942c799e1cf7a516b413d97c0e7c78031db86a
abehan7 <hanjk13262@gmail.com>
2022-08-18 13:27:41 +0900

    chore:> delete console

--- pages/full-page-test.tsx
+++ pages/full-page-test.tsx
@@ -1,11 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { FC, useEffect, useState } from "react";
-// import ReactDOM from "react-dom";
 import ReactFullpage from "@fullpage/react-fullpage";
-import dynamic from "next/dynamic";
-// import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
-// easings.min.js
-// import "fullpage.js/vendors/easings"; // Optional. When using scrollOverflow:true
 import MainBanner from "../components/sections/MainBanner";
 import About from "../components/sections/About";
 import Avatar from "../components/sections/Avatar";
@@ -13,22 +7,8 @@ import Roadmap from "../components/sections/Roadmap";
 import Story from "../components/sections/Story";
 import Faq from "../components/sections/Faq";
 import Wearable from "../components/sections/Wearable";
-import useElementScroll from "../hooks/useElementScroll";
 import { useFooter } from "../contexts/FooterContext";
 import { SECTION_NAME_ARR } from "../constants/navigation";
-
-// NOTE: if using fullpage extensions/plugins put them here and pass it as props.
-const pluginWrapper = () => {
-  require("fullpage.js/vendors/easings.js"); // Optional. Required when using the "scrollHorizontally" extension.
-};
-// import "fullpage.js/vendors/easings"; // Optional. When using scrollOverflow:true
-
-const ComponentsWithNoSSR = dynamic(
-  // typescript에서 props를 전달할때 interface를 정의해줍니다.
-
-  () => import("../components/common/FullpageCssComponent"), // Component로 사용할 항목을 import합니다.
-  { ssr: false } // ssr옵션을 false로 설정해줍니다.
-);
 
 interface Props {
   children: React.ReactNode;
@@ -42,15 +22,13 @@ const MySection: FC<Props> = ({ children }) => {
 };
 
 const ScrollSection: FC<Props> = ({ children }) => {
-  // const { children } = this.props;
-  // const { isScroll, onScroll, scrollTop } = useElementScroll();
   const [container, setContainer] = useState<HTMLElement | null>(null);
   const [scrollPercent, setScrollPercent] = useState<number>(0);
-  // console.log(container?.clientHeight);
   const onScroll = (e: any) => {
     if (!container) return;
     const { scrollTop } = e.target;
-    setScrollPercent(((scrollTop / container?.clientHeight) * 2) / 3);
+    const multiple = 2 / 3;
+    setScrollPercent((scrollTop / container?.clientHeight) * multiple);
   };
 
   useEffect(() => {
@@ -58,15 +36,12 @@ const ScrollSection: FC<Props> = ({ children }) => {
     const dom = document.querySelector(".avatar-container");
     if (!dom) return;
     const scrollLeft = scrollPercent * dom.clientWidth;
-    const scrollWidth = dom.scrollWidth;
-    const scrollLeftPercent = scrollLeft / scrollWidth;
-    // console.log(scrollLeftPercent);
+    // const scrollWidth = dom.scrollWidth;
+    // const scrollLeftPercent = scrollLeft / scrollWidth;
     dom.scrollTo({
       left: scrollLeft,
       behavior: "auto",
     });
-    // document.querySelector(".avatar-container").scrollLeft // 이거는 리덕스든 뭐든 써야할듯
-    // document.querySelector(".avatar-container").scrollWidth
   }, [scrollPercent]);
 
   return (
@@ -82,7 +57,6 @@ const FullpageWrapper = () => {
   const { setCurrentSection } = useFooter();
   return (
     <ReactFullpage
-      pluginWrapper={pluginWrapper}
       anchors={anchors}
       navigation={true}
       navigationTooltips={anchors}
@@ -90,7 +64,6 @@ const FullpageWrapper = () => {
       bigSectionsDestination="bottom"
       scrollOverflowReset={true}
       afterLoad={(origin, destination, direction) => {
-        // console.log(origin, destination, direction);
         setCurrentSection(destination.anchor.toString());
       }}
       render={({ state, fullpageApi }) => {
@@ -127,5 +100,3 @@ const FullpageWrapper = () => {
 };
 
 export default FullpageWrapper;
-
-// ReactDOM.render(<FullpageWrapper />, document.getElementById("react-root"));
