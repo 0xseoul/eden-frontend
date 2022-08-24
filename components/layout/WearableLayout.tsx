c3a5b88f869aaf3031b37ad8ad7ae31950f04f30
abehan7 <hanjk13262@gmail.com>
2022-08-25 00:15:03 +0900

    chore:> moved bg gif to wearable layout

--- components/layout/WearableLayout.tsx
+++ components/layout/WearableLayout.tsx
@@ -7,11 +7,15 @@ interface LayoutProps {
 const styles = {
   layout:
     "flex flex-col max-w-wearable-max h-full w-full justify-center items-center flex-1 mt-header mb-footer",
+  background: `wearable-background absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat bg-center `,
+  overlay: `flex bg-black/80 w-full h-full absolute top-0 left-0 w-screen h-screen z-10`,
 };
 const WearableLayout: FC<LayoutProps> = ({ children }) => {
   return (
     <section className={styles.layout}>
-      <div className="h-full flex-1">{children}</div>
+      <div className="h-full flex-1 z-30">{children}</div>
+      <div className={styles.background} />
+      <div className={styles.overlay} />
     </section>
   );
 };
