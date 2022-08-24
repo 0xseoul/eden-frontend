83c0c15ca4d46ae68b761a82fedf7dec3ae2d339
abehan7 <hanjk13262@gmail.com>
2022-08-24 22:34:46 +0900

    feat:> header dynamic change by pathname

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { MAIN_IMAGES } from "../../constants/image";
 import { HEADER_HEIGHT } from "../../constants/style";
 
 const styles = {
-  section: `min-h-[${HEADER_HEIGHT}] w-full flex] fixed top-0 left-0 right-0 z-50`,
+  section: `min-h-[${HEADER_HEIGHT}] w-full flex] fixed top-0 left-0 right-0 z-50 transition-all`,
   container: `px-[2.5rem] py-[3rem] flex`,
   logo: `cursor-pointer text-lg text-primary font-black `,
 };
 const Header = () => {
+  const router = useRouter();
+  const currNavName = router.pathname;
+  const isWearable = currNavName.includes("/wearable");
   return (
     <section className={styles.section}>
-      <div className={styles.container}>
-        <div className={styles.logo}>EDEN</div>
+      <div className={`${styles.container} ${isWearable && "justify-center"}`}>
+        <div className={`${styles.logo} ${isWearable && "text-white"}`}>
+          <Link href="/">EDEN</Link>
+        </div>
       </div>
     </section>
   );
