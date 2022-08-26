ac52c3dfa07f5c4a49c6ee1b605a1edcb535d4b2
abehan7 <hanjk13262@gmail.com>
2022-08-26 22:50:04 +0900

    feat:> searchbar done

--- components/wearable/Tab.tsx
+++ components/wearable/Tab.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { ICONS } from "../../constants/icons";
+
+const styles = {
+  tab: `flex px-[1.5rem] py-[1rem] w-full justify-between items-center`,
+  container: `flex gap-[1.2rem]`,
+  searchbar: `w-[24rem] h-[3rem] bg-[#202122] flex items-center justify-center relative`,
+  text: ``,
+  input: `w-full px-2`,
+  icon: `absolute right-2`,
+};
 
 const Tab = () => {
-  return <div>tab</div>;
+  return (
+    <div className={styles.tab}>
+      <div className={styles.container}>
+        <span className="font-light">Select your ADAM to customize</span>
+        <span className="font-bold text-primary">5 ADAM(S) Detected</span>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.searchbar}>
+          <input placeholder="search" className={styles.input} />
+          <span className={styles.icon}>
+            <ICONS.search />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tab;
