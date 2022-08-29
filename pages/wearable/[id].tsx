c072a0440da8402fa3d1759b50d620bc5df33d15
abehan7 <hanjk13262@gmail.com>
2022-08-30 07:41:51 +0900

    chore:> clear not using code

--- pages/wearable/[id].tsx
+++ pages/wearable/[id].tsx
@@ -56,23 +56,6 @@ const filterList = [
   },
 ];
 
-interface CardComponentProps {
-  src: string;
-  name: string;
-}
-const CardComponent: FC<CardComponentProps> = ({ src, name }) => {
-  return (
-    <div className={styles.inventoryCard}>
-      <div className="p-2">
-        <div className="w-full object-contain rounded-[8px] overflow-hidden border-[1px] border-primary">
-          <AutoHeightImage src={src} />
-        </div>
-        <div>{name}</div>
-      </div>
-    </div>
-  );
-};
-
 const Wearables = () => {
   const router = useRouter();
   const { id } = router.query;
