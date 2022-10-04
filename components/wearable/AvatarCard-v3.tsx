b5a546ea3cefd04da92b890fd999978aa89c138c
unknown <hanjk13262@gmail.com>
2022-10-05 01:41:30 +0900

    chore:> avatar hover event done

--- components/wearable/AvatarCard-v3.tsx
+++ components/wearable/AvatarCard-v3.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { CSSProperties, FC, useEffect, useMemo, useState } from "react";
 import { WEARABLE_IMAGES } from "../../constants";
 import AutoHeightImage from "../common/AutoHeightImage";
+import { CuttinEdgeBtn } from "../common/Buttons";
 interface Props {
   src: string;
   w?: string;
@@ -9,7 +10,17 @@ interface Props {
 
 const styles = {
   container: `flex flex-col justify-center items-center w-full relative`,
-  overlay: `absolute top-0 left-0 w-full h-full flex justify-center items-center transition-all avatarcard-overlay`,
+  overlay: `absolute top-0 left-0 w-full h-full flex justify-center items-center transition-all avatarcard-overlay duration-300`,
+};
+
+const cssStyles = {
+  downloadBtn: {
+    background:
+      "linear-gradient(92.73deg, rgba(255, 255, 255, 0.24) 0%, rgba(255, 255, 255, 0.04) 100%)",
+    backdropFilter: "blur(32px)",
+    padding: "1rem",
+    wordBreak: "keep-all",
+  } as CSSProperties,
 };
 const AvatarCardV3: FC<Props> = ({ src, w, h }) => {
   const [container, setContainer] = useState<HTMLDivElement | null>(null);
@@ -18,22 +29,28 @@ const AvatarCardV3: FC<Props> = ({ src, w, h }) => {
     return `${w && `w-[${w}]`} ${h && `h-[${h}]`}`;
   };
 
-  const cssStyles = {
-    cardContainer: {
-      width: w,
-      height: h,
-    },
-  };
+  const cardContainerCss = useMemo(() => {
+    return { width: w, height: h };
+  }, [w, h]);
 
   return (
     <div className={styles.container} ref={setContainer}>
       <div
         className={`wearable-card-container-v2 w-full h-full ${size()}`}
-        style={cssStyles.cardContainer}
+        style={cardContainerCss}
       >
         <AutoHeightImage src={WEARABLE_IMAGES["avatar-frame"]} />
         <AutoHeightImage src={src} />
-        <div className={styles.overlay} style={{ zIndex: 2 }} />
+        <div className={styles.overlay} style={{ zIndex: 2 }}>
+          <span className="w-[80%] h-full flex items-center justify-center opacity-0">
+            <CuttinEdgeBtn
+              cssStyles={cssStyles.downloadBtn}
+              tw="-translate-y-[50%]"
+            >
+              VIEW
+            </CuttinEdgeBtn>
+          </span>
+        </div>
       </div>
     </div>
   );
