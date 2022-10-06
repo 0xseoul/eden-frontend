bde97b30d9a6b57c737c4e23a9dbe2c5d334aeb0
unknown <hanjk13262@gmail.com>
2022-10-06 23:42:25 +0900

    add:> avatar

--- components/wearable/[id]/ItemsContainer.tsx
+++ components/wearable/[id]/ItemsContainer.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from "react";
 import { ICONS } from "../../../constants";
-import { useWearableDetailPageContext } from "../../../pages/wearable/[id]";
 
 const styles = {
   inventoryContainer: `flex-1 flex h-full overflow-y-auto wearable-custom-scrollbar- `,
   inventoryWrapper: `flex-1 flex h-full flex-col`,
   grid: `grid grid-cols-3 auto-rows-[270px] w-full`,
+  btnText: `uppercase btn-text h-[3rem] flex-1 flex items-center justify-center font-black text-c-gray300`,
+  icon: `btn- h-[3rem] w-[3rem] flex items-center justify-center transition-all`,
+  btnWrapper: `flex w-[228px] wearable-reset-button overflow-hidden cursor-pointer`,
 };
 
 interface Props {
@@ -13,6 +15,7 @@ interface Props {
   handleToggleLock: () => void;
   isResetClicked: boolean;
 }
+
 const ItemsContainer: FC<Props> = ({
   children,
   handleToggleLock,
@@ -25,16 +28,14 @@ const ItemsContainer: FC<Props> = ({
       </form>
       <div>
         <div className="flex justify-end w-full">
-          <div className="flex w-[228px] wearable-reset-button overflow-hidden cursor-pointer">
+          <div className={styles.btnWrapper}>
             <span
-              className={`btn- h-[3rem] w-[3rem] flex items-center justify-center ${
-                isResetClicked && "bg-primary"
-              } transition-all`}
+              className={`${styles.icon} ${isResetClicked && "bg-primary"} `}
               onClick={handleToggleLock}
             >
               {isResetClicked ? <ICONS.lockOpen /> : <ICONS.lock />}
             </span>
-            <div className="uppercase btn-text h-[3rem] flex-1 flex items-center justify-center font-black text-c-gray300">
+            <div className={styles.btnText}>
               <span className="translate-y-0.5">RESET</span>
             </div>
           </div>
