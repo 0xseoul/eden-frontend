d30be1ba3b82f43f87a4b8740be4015b6ba8b5f2
unknown <hanjk13262@gmail.com>
2022-10-29 23:54:41 +0900

    Chore:> chagned signedUrl to fake url

--- pages/api/file.ts
+++ pages/api/file.ts
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { S3Client, AbortMultipartUploadCommand } from "@aws-sdk/client-s3";
-import { getObjectSignedUrl } from "../../utils/s3";
 
 type Data = string;
 
@@ -10,7 +8,8 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-    const endpoint = await getObjectSignedUrl("0xSEOUL_TEST.unitypackage");
+    const endpoint = "tmp";
+    // const endpoint = await getObjectSignedUrl("0xSEOUL_TEST.unitypackage");
     res.status(200).send(endpoint);
   } catch (error: any) {
     res.status(500).send(error.message);
